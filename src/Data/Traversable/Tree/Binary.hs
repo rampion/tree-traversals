0f839dbd92aa5cd3c76eb332965fbcd525161a6b
Noah Luck Easterly <noah.easterly@gmail.com>
2018-03-15 16:04:25 -0400

    Remove examples from Data.Tree.Binary

--- src/Data/Traversable/Tree/Binary.hs
+++ src/Data/Traversable/Tree/Binary.hs
@@ -1,15 +1,18 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+-- |
+-- Different traversals for 'Data.Tree.Binary.Tree' from "Data.Tree.Binary".
+--
 module Data.Traversable.Tree.Binary 
   ( inorder, InOrder(..)
   , preorder, PreOrder(..)
   , postorder, PostOrder(..)
   , levelorder, LevelOrder(..)
+  , rlevelorder, RLevelOrder(..)
   ) where
 
--- import Control.Applicative.Backwards (Backwards(..))
--- import Data.Functor.Compose (Compose(..))
 import Data.Traversable (foldMapDefault)
 
 import Control.Applicative.Batch
@@ -22,14 +25,30 @@ import Data.Tree.Binary
 -- | 
 -- Traverse the nodes of a tree from left-most to right-most.
 --
--- >>> _ <- inorder print $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> _ <- inorder print example
 -- [L,L]
+-- [L,L,R]
 -- [L]
+-- [L,R,L]
+-- [L,R,L,R]
 -- [L,R]
 -- []
--- [R,L]
 -- [R]
+-- [R,R,L]
 -- [R,R]
+-- [R,R,R]
 inorder :: Applicative f => (a -> f b) -> Tree a -> f (Tree b)
 inorder _ Leaf = pure Leaf
 inorder f (Branch a la ra) = (\lb b rb -> Branch b lb rb) <$> inorder f la <*> f a <*> inorder f ra
@@ -37,14 +56,30 @@ inorder f (Branch a la ra) = (\lb b rb -> Branch b lb rb) <$> inorder f la <*> f
 -- | 
 -- Traverse each node of the tree, then its left-subtree, then its right-subtree.
 --
--- >>> _ <- preorder print $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> _ <- preorder print example
 -- []
 -- [L]
 -- [L,L]
+-- [L,L,R]
 -- [L,R]
+-- [L,R,L]
+-- [L,R,L,R]
 -- [R]
--- [R,L]
 -- [R,R]
+-- [R,R,L]
+-- [R,R,R]
 preorder :: Applicative f => (a -> f b) -> Tree a -> f (Tree b)
 preorder _ Leaf = pure Leaf
 preorder f (Branch a la ra) = Branch <$> f a <*> preorder f la <*> preorder f ra
@@ -52,11 +87,27 @@ preorder f (Branch a la ra) = Branch <$> f a <*> preorder f la <*> preorder f ra
 -- | 
 -- Traverse each node after traversing its left and right subtrees.
 --
--- >>> _ <- postorder print $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> _ <- postorder print example
+-- [L,L,R]
 -- [L,L]
+-- [L,R,L,R]
+-- [L,R,L]
 -- [L,R]
 -- [L]
--- [R,L]
+-- [R,R,L]
+-- [R,R,R]
 -- [R,R]
 -- [R]
 -- []
@@ -69,14 +120,30 @@ postorder f (Branch a la ra) = (\lb rb b -> Branch b lb rb) <$> postorder f la <
 -- nodes of depth zero, then all nodes of depth 1, then all nodes of depth 2,
 -- etc.)
 --
--- >>> _ <- levelorder print $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> _ <- levelorder print example
 -- []
 -- [L]
 -- [R]
 -- [L,L]
 -- [L,R]
--- [R,L]
 -- [R,R]
+-- [L,L,R]
+-- [L,R,L]
+-- [R,R,L]
+-- [R,R,R]
+-- [L,R,L,R]
 levelorder :: Applicative f => (a -> f b) -> Tree a -> f (Tree b)
 levelorder f = topDown $ \case
   Leaf           -> pure Leaf
@@ -87,10 +154,26 @@ levelorder f = topDown $ \case
 -- nodes of depth n, then all nodes of depth n-1, then all nodes of depth n-2,
 -- etc.)
 --
--- >>> _ <- rlevelorder print $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> _ <- rlevelorder print example
+-- [L,R,L,R]
+-- [L,L,R]
+-- [L,R,L]
+-- [R,R,L]
+-- [R,R,R]
 -- [L,L]
 -- [L,R]
--- [R,L]
 -- [R,R]
 -- [L]
 -- [R]
@@ -100,33 +183,32 @@ rlevelorder f = bottomUp $ \case
   Leaf           -> pure Leaf
   Branch a la ra -> Branch <$> lift (f a) <*> batch la <*> batch ra
 
-{-
-type Search = Compose ((,) (Last Direction))
-
-dfs :: Applicative f => (a -> Search f b) -> Tree a -> Search f (Tree b)
-dfs _ Leaf = pure Leaf
-dfs f (Branch a la ra) = let fb@(Compose (Last d, _)) = f a in case d of
-  L -> Branch <$> fb <*> dfs f la <*> dfs f ra
-  R -> (flip . Branch) <$> fb <*> dfs f ra <*> dfs f la
-
-bfs :: Applicative f => (a -> Search f b) -> Tree a -> Search f (Tree b)
-bfs f = runWithQueue $ \case
-  Leaf -> pure Leaf
-  Branch a la ra -> let fb@(Compose (Last d, _)) = f a in case d of
-    L -> Branch <$> lift fb <*> batch la <*> batch ra
-    R -> (flip . Branch) <$> lift fb <*> batch la <*> batch ra
-    -}
-    
 -- | 'Tree' wrapper to use 'inorder' traversal
 --
--- >>> mapM_ print . InOrder $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> mapM_ print $ InOrder example
 -- [L,L]
+-- [L,L,R]
 -- [L]
+-- [L,R,L]
+-- [L,R,L,R]
 -- [L,R]
 -- []
--- [R,L]
 -- [R]
+-- [R,R,L]
 -- [R,R]
+-- [R,R,R]
 newtype InOrder a = InOrder { getInOrder :: Tree a }
   deriving Functor
 instance Foldable InOrder where
@@ -136,17 +218,30 @@ instance Traversable InOrder where
 
 -- | 'Tree' wrapper to use 'preorder' traversal
 --
--- >>> mapM_ print . PreOrder $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> mapM_ print $ PreOrder example
 -- []
 -- [L]
 -- [L,L]
+-- [L,L,R]
 -- [L,R]
+-- [L,R,L]
+-- [L,R,L,R]
 -- [R]
--- [R,L]
 -- [R,R]
--- >>> import Data.Foldable (toList)
--- >>> take 4 . toList $ PreOrder allDepths
--- [ [] , [ L ] , [ L , L ] , [ L , L , L ] ]
+-- [R,R,L]
+-- [R,R,R]
 newtype PreOrder a = PreOrder { getPreOrder :: Tree a }
   deriving Functor
 instance Foldable PreOrder where
@@ -156,11 +251,27 @@ instance Traversable PreOrder where
 
 -- | 'Tree' wrapper to use 'postorder' traversal
 --
--- >>> mapM_ print . PostOrder $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> mapM_ print $ PostOrder example
+-- [L,L,R]
 -- [L,L]
+-- [L,R,L,R]
+-- [L,R,L]
 -- [L,R]
 -- [L]
--- [R,L]
+-- [R,R,L]
+-- [R,R,R]
 -- [R,R]
 -- [R]
 -- []
@@ -173,17 +284,30 @@ instance Traversable PostOrder where
 
 -- | 'Tree' wrapper to use 'levelorder' traversal
 --
--- >>> mapM_ print . LevelOrder $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> mapM_ print $ LevelOrder example
 -- []
 -- [L]
 -- [R]
 -- [L,L]
 -- [L,R]
--- [R,L]
 -- [R,R]
--- >>> import Data.Foldable (toList)
--- >>> take 4 . toList $ LevelOrder allDepths
--- [ [] , [ L ] , [ R ] , [ L , L ] ]
+-- [L,L,R]
+-- [L,R,L]
+-- [R,R,L]
+-- [R,R,R]
+-- [L,R,L,R]
 newtype LevelOrder a = LevelOrder { getLevelOrder :: Tree a }
   deriving Functor
 instance Foldable LevelOrder where
@@ -193,10 +317,26 @@ instance Traversable LevelOrder where
 
 -- | 'Tree' wrapper to use 'rlevelorder' traversal
 --
--- >>> mapM_ print . RLevelOrder $ toDepth 3
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+-- >>> mapM_ print $ RLevelOrder example
+-- [L,R,L,R]
+-- [L,L,R]
+-- [L,R,L]
+-- [R,R,L]
+-- [R,R,R]
 -- [L,L]
 -- [L,R]
--- [R,L]
 -- [R,R]
 -- [L]
 -- [R]
@@ -207,3 +347,134 @@ instance Foldable RLevelOrder where
   foldMap = foldMapDefault
 instance Traversable RLevelOrder where
   traverse f = fmap RLevelOrder . rlevelorder f . getRLevelOrder
+
+{------------------------------------------------------------------------------
+ - EXAMPLE HELPER FUNCTIONS
+ ------------------------------------------------------------------------------}
+
+data Direction = L | R
+  deriving (Show, Eq, Enum)
+
+type Path = [Direction]
+
+-- |
+-- >>> prettyPrint Leaf
+-- ●
+-- >>> prettyPrint (Branch [] Leaf $ Branch [R] Leaf $ Branch [R,R] Leaf Leaf)
+--     []
+-- ┌───┴───┐
+-- ●      [R]
+--      ┌──┴──┐
+--      ●   [R,R]
+--           ┌┴┐
+--           ● ●
+prettyPrint :: Show a => Tree a -> IO ()
+prettyPrint = \t -> let (_,_,xs) = draw t in mapM_ putStrLn xs where
+  draw :: Show a => Tree a -> (Int, Int, [String])
+  draw Leaf = (1, 0, ["●"])
+  draw (Branch a l r) = (w, c, top : next : outerMeld lb rb)
+    where (lw,lc,lb) = draw l
+          (rw,rc,rb) = draw r
+          -- make both branches equidistant from the center
+          rc' = max rc (lw - 1 - lc)
+          rw' = rw - rc + rc'
+          lw' = lc + 1 + rc'
+
+          as = show a
+          aw = length as
+          alw = (aw - 1) `div` 2
+          arw = aw - 1 - alw
+          c = max lw' alw
+          w = c + 1 + max rw' arw
+
+          top = replicate (c - alw) ' ' ++ as
+          next = concat
+            [ replicate (c - lw' + lc) ' '
+            , "┌"
+            , replicate (lw' - 1 - lc) '─'
+            , "┴"
+            , replicate rc' '─'
+            , "┐"
+            ]
+          meld ls rs = concat
+            [ replicate (c - lw') ' '
+            , take (lw' + 1) $ ls ++ repeat ' '
+            , replicate (rc' - rc) ' '
+            , rs
+            ]
+          outerMeld (x:xs) (y:ys) = meld x y : outerMeld xs ys
+          outerMeld (x:xs) [] = meld x "" : outerMeld xs []
+          outerMeld [] (y:ys) = meld "" y : outerMeld [] ys
+          outerMeld [] [] = []
+
+-- |
+-- >>> prettyPrint balanced
+--            []
+--      ┌─────┴─────┐
+--     [L]         [R]
+--   ┌──┴──┐     ┌──┴──┐
+-- [L,L] [L,R] [R,L] [R,R]
+--  ┌┴┐   ┌┴┐   ┌┴┐   ┌┴┐ 
+--  ● ●   ● ●   ● ●   ● ● 
+balanced :: Tree Path
+balanced =
+  Branch []
+    ( Branch [L]
+      ( Branch [L,L] Leaf Leaf )
+      ( Branch [L,R] Leaf Leaf )
+    )
+    ( Branch [R]
+      ( Branch [R,L] Leaf Leaf )
+      ( Branch [R,R] Leaf Leaf )
+    )
+
+
+-- |
+-- >>> prettyPrint example
+--                                                []
+--                     ┌──────────────────────────┴──────────────────────────┐
+--                    [L]                                                   [R]
+--     ┌───────────────┴───────────────┐                             ┌───────┴───────┐
+--   [L,L]                           [L,R]                           ●             [R,R]
+-- ┌───┴───┐                 ┌─────────┴─────────┐                               ┌───┴───┐
+-- ●    [L,L,R]           [L,R,L]                ●                            [R,R,L] [R,R,R]
+--        ┌┴┐           ┌────┴────┐                                             ┌┴┐     ┌┴┐
+--        ● ●           ●     [L,R,L,R]                                         ● ●     ● ●
+--                               ┌┴┐                                 
+--                               ● ●                                 
+example :: Tree Path
+example = 
+  Branch []
+  ( Branch [L]
+    ( Branch [L,L]
+      Leaf
+      ( Branch [L,L,R]
+        Leaf
+        Leaf
+      )
+    )
+    ( Branch [L,R]
+      ( Branch [L,R,L]
+        Leaf 
+        ( Branch [L,R,L,R]
+          Leaf
+          Leaf
+        )
+      )
+      Leaf
+    )
+  )
+  ( Branch [R]
+    Leaf
+    ( Branch [R,R]
+      ( Branch [R,R,L]
+        Leaf
+        Leaf
+      )
+      ( Branch [R,R,R]
+        Leaf
+        Leaf
+      )
+    )
+  )
+
